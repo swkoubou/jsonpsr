9515ea3aad91fdf4e434484ff523af28c8f3a41c
x0y14 <ya01ma08@gmail.com>
2022-04-21 11:59:08 +0900

    関数プレースホルダー

--- parser/parser.go
+++ parser/parser.go
@@ -15,4 +15,74 @@ func NewParser() *Parser {
 	}
 }
 
-func (p *Parser) Parse() {}
+// トークナイザと同じ。走査(?)に必要な基本機能。
+func (p *Parser) curt() tokenizer.Token {
+	return p.input[p.pos]
+}
+func (p *Parser) prev() tokenizer.Token {
+	return p.input[p.pos-1]
+}
+func (p *Parser) next() tokenizer.Token {
+	return p.input[p.pos+1]
+}
+func (p *Parser) goNext() {
+	p.pos++
+}
+func (p *Parser) isEof() bool {
+	return p.pos >= len(p.input)
+}
+
+func (p *Parser) Parse() []*Node {
+	return nil
+}
+
+func (p *Parser) json() *Node {
+	// json = element
+	return p.element()
+}
+func (p *Parser) element() *Node {
+	// element = value
+	return p.value()
+}
+func (p *Parser) value() *Node {
+	// value = object | array | string | number | "true" | "false" | "null"
+	return nil
+}
+
+func (p *Parser) object() *Node {
+	// object = "{" members? "}"
+	return nil
+}
+func (p *Parser) members() *Node {
+	// members = member ("," member)*
+	return nil
+}
+func (p *Parser) member() *Node {
+	// member = string ":" element
+	return nil
+}
+
+func (p *Parser) array() *Node {
+	// array = "[" elements? "]"
+	return nil
+}
+func (p *Parser) elements() *Node {
+	// elements = element ("," element)*
+	return nil
+}
+
+func (p *Parser) string() *Node {
+	return nil
+}
+func (p *Parser) number() *Node {
+	return nil
+}
+func (p *Parser) true() *Node {
+	return nil
+}
+func (p *Parser) false() *Node {
+	return nil
+}
+func (p *Parser) null() *Node {
+	return nil
+}
