a864e3b49dccbc7a8b5e240ea33886e1c11ab3c7
x0y14 <ya01ma08@gmail.com>
2022-04-21 12:29:01 +0900

    elementsや、membersは複数のノードを所持するので、ポインタではなく、アレイに。

--- parser/node.go
+++ parser/node.go
@@ -3,6 +3,7 @@ package parser
 import "github.com/swkoubou/jsonpsr/tokenizer"
 
 type Node struct {
-	tokens []tokenizer.Token
-	child  *Node
+	Kind
+	tokens   []tokenizer.Token
+	children []Node
 }
